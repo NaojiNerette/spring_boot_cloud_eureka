353884f5c71af5c6e0aeb530f59a4b8ddf2f461a
Joan Bascur <naojimagot@gmail.com>
2023-06-29 17:22:11 -0400

    proyecto commons

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/clientes/ProductoClienteRest.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/clientes/ProductoClienteRest.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import com.formacionbdi.springboot.app.item.models.Producto;
+import com.formacionbdi.springboot.app.commons.models.entity.Producto;
 
 @FeignClient(name = "servicio-productos")
 public interface ProductoClienteRest {
--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formacionbdi.springboot.app.item.models.Item;
-import com.formacionbdi.springboot.app.item.models.Producto;
+import com.formacionbdi.springboot.app.commons.models.entity.Producto;
 import com.formacionbdi.springboot.app.item.models.service.ItemService;
 
 @RefreshScope
