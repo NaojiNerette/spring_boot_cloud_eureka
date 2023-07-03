a9bd6218e40c62c4aa5c9aec10232037092e2df1
NaojiNerette <naojimagot@gmail.com>
2023-07-03 19:25:21 -0400

    implementacion servicio oauth

---
+++ springboot-servicio-oauth/src/main/java/com/formacionbdi/springboot/app/oauth/SpringbootServicioOauthApplication.java
@@ -0,0 +1,36 @@
+package com.formacionbdi.springboot.app.oauth;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SpringbootServicioOauthApplication implements CommandLineRunner{
+
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioOauthApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		String password = "12345";
+		
+		for (int i = 0; i < 4; i++) {
+			String passwordBCrypt = passwordEncoder.encode(password);
+			System.out.println(passwordBCrypt);
+		}
+	}
+
+}
