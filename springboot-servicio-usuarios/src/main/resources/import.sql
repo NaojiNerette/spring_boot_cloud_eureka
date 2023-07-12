ded8c895d260c7bc546c02110be9964d1d1c8e3d
NaojiNerette <naojimagot@gmail.com>
2023-07-11 22:37:54 -0400

    pass actualizadas

--- springboot-servicio-usuarios/src/main/resources/import.sql
+++ springboot-servicio-usuarios/src/main/resources/import.sql
@@ -1,6 +1,6 @@
-INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('jbascur', '123456', 1, 'Joan', 'Bascur', 'correo_jb@gmail.com');
-INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('jvasques', '123456', 1, 'Juan', 'Vasques', 'correo_jv@gmail.com');
-INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('mcerda', '123456', 1, 'Miguel', 'Cerda', 'correo_mc@gmail.com');
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('jbascur', '$2a$10$bDb7hmi.tsKGEpLKLfkmzeChybTyBFR6KVrBs0nJLARWT.5eANXHO', 1, 'Joan', 'Bascur', 'correo_jb@gmail.com');
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('jvasques', '$2a$10$8O/ZWfjuwkRfC9V5mdB4sulLjS8CVrUGngaB/YqIWcyoqt1qamCdy', 1, 'Juan', 'Vasques', 'correo_jv@gmail.com');
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('mcerda', '$2a$10$QumQOOfYBkFJZGDlAysiN..tL.wo0/vPzQfBsqAQEox9HRDAayUVG', 1, 'Miguel', 'Cerda', 'correo_mc@gmail.com');
 
 INSERT INTO `roles` (nombre) VALUES ('ROLE_USER');
 INSERT INTO `roles` (nombre) VALUES ('ROLE_ADMIN');
